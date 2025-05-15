b16f31e03b36d6973b182a5f0605c33ac5485463
Uriel Mora <upm1808@gmail.com>
2025-05-15 16:13:35 -0600

    Feat OneToMany and ManyToOne

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -3,8 +3,10 @@ import {
   BeforeUpdate,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ProductImage } from './product-image.entity';
 
 @Entity()
 export class Product {
@@ -52,6 +54,13 @@ export class Product {
   })
   tags: string[];
 
+  @OneToMany(
+    () => ProductImage,
+    (productImage) => productImage.product,
+    {cascade: true}
+  )
+  images?: ProductImage;
+
   @BeforeInsert()
   checkSlugInsert() {
     if (!this.slug) {
@@ -71,3 +80,4 @@ export class Product {
       .replaceAll("'", '');
   }
 }
+ 
