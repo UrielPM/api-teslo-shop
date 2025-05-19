cd6c0bf4f06bfbe1594b4f3c5015b57d49493f05
Uriel Mora <upm1808@gmail.com>
2025-05-19 10:59:03 -0600

    Feat: cascade elimination

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -138,4 +138,13 @@ export class ProductsService {
       'Unexpected error, check server logs!',
     );
   }
+
+  async deleteAllProducts() {
+    const query = this.productRepository.createQueryBuilder('product');
+    try {
+      return await query.delete().where({}).execute;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
+  }
 }
