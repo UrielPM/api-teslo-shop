44f2bf33be660b666c9937a8e5e5b3787b650562
Uriel Mora <upm1808@gmail.com>
2025-06-16 12:40:45 -0600

    Feat: Serving files in a controlled manner

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException } from '@nestjs/common';
+import { Controller, Get, Post, Param, UploadedFile, UseInterceptors, BadRequestException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
+import { Response } from 'express';
 
 
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  finProductImage(@Res() res: Response, @Param('imageName') imageName: string) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -27,8 +34,10 @@ export class FilesController {
     if (!file) {
       throw new BadRequestException('Make sure that the file is an image');
     }
+
+    const secureUrl = `${file.filename}`;
     return {
-      fileName: file.originalname,
+      secureUrl,
     };
   }
 }
