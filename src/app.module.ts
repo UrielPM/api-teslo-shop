09417c14efbd7e104541bffe8f58b401092f8d3a
Uriel Mora <upm1808@gmail.com>
2025-06-16 13:35:34 -0600

    Feat: Other ways to deploy files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -14,10 +16,13 @@ import { FilesModule } from './files/files.module';
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT ?? '5432', 10),
       database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME, 
-      password: process.env.DB_PASSWORD, 
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     ProductsModule,
     CommonModule,
